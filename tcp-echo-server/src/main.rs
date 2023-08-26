9ce36adab9485299bc0ea68f5819858eb2047174
anhongkui <anhongkui@jd.com>
2023-08-26 21:08:43 +0800

    tcp echo server

---
+++ tcp-echo-server/src/main.rs
@@ -0,0 +1,35 @@
+use std::io::{Read, Write};
+use std::net::{Shutdown, TcpListener, TcpStream};
+use std::thread;
+
+fn handle_client(mut stream: TcpStream) {
+    let mut data = [0 as u8, 255];
+    while match stream.read(&mut data) {
+        Ok(size) => {
+            stream.write(&data[0..size]).unwrap();
+            true
+        }
+        Err(_) => {
+            stream.shutdown(Shutdown::Both).unwrap();
+            false
+        }
+    } {}
+}
+
+fn main() {
+    let addr = "0.0.0.0:33333";
+    let listener = TcpListener::bind(addr).unwrap();
+    println!("server listen on {}", addr);
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("New connection: {}", stream.peer_addr().unwrap());
+                thread::spawn(move || handle_client(stream));
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    }
+}
