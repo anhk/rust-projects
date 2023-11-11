26f12964808a5e4248895bbba099706efed8d683
anhongkui <anhongkui@jd.com>
2023-11-11 22:38:40 +0800

    update

---
+++ args/src/main.rs
@@ -0,0 +1,21 @@
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    name: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    println!("args.name: {0}", args.name);
+    println!("args.count: {0}", args.count);
+}
