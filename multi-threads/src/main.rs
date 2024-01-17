91371a7740e04bb40bd89eaed5c2486d1bef621b
anhongkui <anhongkui@jd.com>
2024-01-17 15:01:54 +0000

    multi threads

--- multi-threads/src/main.rs
+++ multi-threads/src/main.rs
@@ -1,3 +1,17 @@
+use std::thread::{self, JoinHandle};
+
 fn main() {
-    println!("Hello, world!");
+    let mut handles = Vec::new();
+    for i in 0..5 {
+        println!("Hello, world {}!", i);
+        handles.push(thread::spawn(move || inthread(i)));
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+}
+
+fn inthread(i: i32) {
+    println!("in thread {}", i)
 }
